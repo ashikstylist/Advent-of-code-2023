79a5fca71d2df435616ec9f0717cb0efa906a16e
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2024-02-08 11:56:02 +0530

    Update and rename 01.md to 01.ts

--- 01.ts
+++ 01.ts
@@ -1,15 +1,15 @@
 const encryptedValue: string = ``; // Input
 
-const enc_list = encryptedValue.split("\n");
-let count = 0;
+const enc_list : string[] = encryptedValue.split("\n");
+let count: number = 0;
 
 enc_list.map(data => {
-  let num = "";
+  let num: string = "";
   data.split("").filter(res => {
     parseInt(res) && (num += res)
   })
 
-  const decypt_num = num.split("")[0] + num.split("")[num.length - 1]
+  const decypt_num: string[] = num.split("")[0] + num.split("")[num.length - 1]
   count += parseInt(decypt_num)
 })
 
